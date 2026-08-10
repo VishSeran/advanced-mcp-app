7ad6f1281ea0b8ac84f0eeba9669f4bd987606f3
VishSeran <vishwa.sovis@gmail.com>
2026-08-10 12:57:23 +0530

    is_within_roots updated

--- configurations/configs.py
+++ configurations/configs.py
@@ -1,12 +1,16 @@
-from configurations.logger import get_logger
+from pathlib import Path
 
+from configurations.logger import get_logger
+from server.mcp_server import base_dir
 
 logger = get_logger("configs")
 
-def is_within_roots(path):
+def is_within_roots(path:Path) -> bool:
     
     try:
-        
-    except Exception as e:
-        logger.error(f"Error in is_within_roots: {e}")
+        path.resolve().relative_to(base_dir.resolve())
+        return True
+    
+    except ValueError:
+        return False
         
