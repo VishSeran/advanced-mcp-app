34640b765a95feca7f7af0524cb16b76f8662018
VishSeran <vishwa.sovis@gmail.com>
2026-08-11 19:27:53 +0530

    mcp host app-init updated

--- mcp_http_host_app.py
+++ mcp_http_host_app.py
@@ -1,13 +1,16 @@
+from langchain_mcp_adapters.tools import load_mcp_tools
 
 from configurations.logger import get_logger
 from client.mcp_client import MCPHTTPClient
+from agents.llm_agent import LLMAgent
 
 logger = get_logger("http-app")
 
 class MCPHTTPHostApp(MCPHTTPClient):
     
-    def __init__(self, server_url, roots_dir):
+    async def __init__(self, server_url, roots_dir):
         super().__init__(server_url, roots_dir)
         
         self.conversations = []
-        
+        self.tools = []
+        self.llm_client = None
